c651123659c63b9c4ae21c41993405ac7dffccce
surainfernando <59121315+surainfernando@users.noreply.github.com>
2020-03-06 13:59:53 +0530

    Surain -First route, home rout

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Homepage! Hello world.');
-});
+const home = require("./routes/home");
 
-app.get('/about', (req, res) => { // new
-  res.send('About page. Nice.');
-});
 
-app.listen(3000, () => console.log('listening on port 3000'));
+
+app.use("/",home);
+
+app.listen(8000, () => console.log('listening on port 8000'));
