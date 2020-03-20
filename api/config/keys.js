df98c047fb91828a005eac92a021872fe4c8b081
Waruna-Kaushalya <waruna95610@gmail.com>
2020-03-21 00:10:46 +0530

    add mongodb env variable

--- api/config/keys.js
+++ api/config/keys.js
@@ -1,6 +1,8 @@
 var mongooseconn = require('mongoose');
 
-mongooseconn.connect('mongodb+srv://Waruna:1qaz@shopifirecluster-qkn98.mongodb.net/shopifire?retryWrites=true&w=majority', function (error) 
+const db = 'mongodb+srv://Waruna:1qaz@shopifirecluster-qkn98.mongodb.net/shopifire?retryWrites=true&w=majority'
+
+mongooseconn.connect(process.env.MONGODB_URL || db, function (error) 
 
 {
     if (error == null) {
