50fb08c2d5a1b43bb1f160cceed5437a5affa0e9
Waruna-Kaushalya <waruna95610@gmail.com>
2020-03-20 11:31:51 +0530

    add mongodb atlas

--- api/config/keys.js
+++ api/config/keys.js
@@ -1,5 +1,11 @@
 var mongooseconn = require('mongoose');
-mongooseconn.connect('mongodb://localhost/shopifireDB', function (error) {
+// mongooseconn.connect('mongodb://localhost/shopifireDB', function (error) 
+
+mongooseconn.connect('mongodb+srv://Waruna:1qaz@cluster0-mdsj9.mongodb.net/shopifiredb?retryWrites=true&w=majority', function (error) 
+
+
+
+{
     if (error == null) {
         console.log("Connected");
     }
