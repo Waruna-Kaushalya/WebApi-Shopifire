1960b8c6374ab7aa5becd4a8b11b7d64cf33f851
Waruna-Kaushalya <waruna95610@gmail.com>
2020-03-20 13:27:30 +0530

    update config data

--- api/config/keys.js
+++ api/config/keys.js
@@ -1,8 +1,9 @@
 var mongooseconn = require('mongoose');
 
 
-mongooseconn.connect('mongodb+srv://Waruna:1qaz@cluster0-mdsj9.mongodb.net/shopifiredb?retryWrites=true&w=majority', function (error) 
+// mongooseconn.connect('mongodb+srv://Waruna:1qaz@cluster0-mdsj9.mongodb.net/shopifiredb?retryWrites=true&w=majority', function (error) 
 
+mongooseconn.connect(process.env.MONGODB_URL, function (error) 
 
 
 {
