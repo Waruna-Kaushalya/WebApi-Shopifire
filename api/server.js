46bed567eb499beb1cfbc347ae8653beb4ab6268
Waruna-Kaushalya <waruna95610@gmail.com>
2020-03-21 13:11:23 +0530

    updtae app

--- api/server.js
+++ api/server.js
@@ -104,3 +104,9 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
 console.log('api runnging on port ' + PORT + ': ');
+
+
+// app.listen(PORT, '0.0.0.0', function(err) {
+//   console.log("===========================")
+//   console.log('api runnging on port ' + PORT + ': ');
+// });
